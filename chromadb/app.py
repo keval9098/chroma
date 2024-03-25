9112da25d64a33c5f5fbec2cd0f381a2176edffa
keval9098 <kevalkant9098@gmail.com>
2024-03-25 09:51:08 +0530

    Added static files

--- chromadb/app.py
+++ chromadb/app.py
@@ -1,7 +1,19 @@
 import chromadb
 import chromadb.config
 from chromadb.server.fastapi import FastAPI
+from fastapi import applications
+from fastapi.staticfiles import StaticFiles
+from fastapi.openapi.docs import get_swagger_ui_html
 
 settings = chromadb.config.Settings()
 server = FastAPI(settings)
 app = server.app()
+app.mount("/static", StaticFiles(directory="./static"), name="static")
+def swagger_monkey_patch(*args, **kwargs):
+    return get_swagger_ui_html(
+        *args,
+        **kwargs,
+        swagger_js_url="/static/bundle.js",
+        swagger_css_url="/static/swaggerui.css",
+    )
+applications.get_swagger_ui_html = swagger_monkey_patch
